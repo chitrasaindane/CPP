3fd9085f11e81dfe8a8c4a275aecbbe8ca431a61
Chitra Saindane <163283978+chitrasaindane@users.noreply.github.com>
2024-09-21 09:50:59 +0000

    delete element

---
+++ array/delete.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct array
+{
+    int a[10];
+    int size;
+    int length;
+};
+
+void display(struct array ar)
+{
+    for (int i = 0; i < ar.length; i++)
+    {
+        cout << ar.a[i] << " ";
+    }
+    cout << endl;
+}
+
+void Delete(struct array *ar, int index)
+{
+    for (int i = index; i < ar->length - 1; i++)
+    {
+        ar->a[i] = ar->a[i + 1];
+    }
+    ar->length--;
+}
+
+int main()
+{
+    struct array arr = {{1, 2, 3, 4, 5, 6, 7}, 10, 7};
+    display(arr);
+    Delete(&arr , 1);
+    display(arr);
+}
