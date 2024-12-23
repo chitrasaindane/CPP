a0669f3a2ef7439a064bc0d726b3540b643896d1
Chitra Saindane <163283978+chitrasaindane@users.noreply.github.com>
2024-12-23 18:04:00 +0000

    Create Node and Display linked list element

---
+++ linked list/display_linked_list.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;         
+    Node* next;       
+};
+
+Node* head = nullptr ;
+Node* temp;
+Node* createNode(int data) {
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->next = nullptr;
+    
+
+    if(head == nullptr)
+    {   head= newNode;
+        temp = head;
+       
+    }
+    else{
+     temp -> next = newNode;
+     temp = temp->next;
+    }
+}
+
+
+void displayLinkedList() {
+    Node* current = head; 
+    while (current != nullptr) {
+        cout << current->data << " -> ";
+        current = current->next; 
+    }
+    cout << "NULL" << endl; 
+}
+
+int main() {
+    
+    createNode(10); 
+    createNode(20); 
+    createNode(30); 
+    createNode(40);
+
+   
+    cout << "Linked list elements: ";
+    displayLinkedList();
+
+    
+    Node* temp;
+    while (head != nullptr) {
+        temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    return 0;
+}
