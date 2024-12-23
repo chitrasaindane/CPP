f589f119746cd93d2c992872883ddec9d5854811
Chitra Saindane <163283978+chitrasaindane@users.noreply.github.com>
2024-12-23 18:37:37 +0000

    Sum of element in linked list

---
+++ linked list/sum_linked_list_ele.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;         
+    Node* next;       
+};
+
+Node* head = nullptr ;
+Node* temp;
+Node* createNode(int data) {
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->next = nullptr;
+    
+
+    if(head == nullptr)
+    {   head= newNode;
+   
+    }
+    else{
+      temp=head;
+     while (temp->next != nullptr) {
+            temp = temp->next;   
+        }
+        temp->next = newNode;
+    }
+}
+
+int Sum_ele(){
+     Node*p = head;
+     int sum = 0; 
+     while(p != nullptr){
+     sum += p->data;
+     p=p->next;
+    } 
+    return sum;
+}
+
+int main() {
+    
+    createNode(10); 
+    createNode(20); 
+    createNode(30); 
+    createNode(40);
+
+     
+
+    cout << "Sum of Element : ";
+    cout<<Sum_ele();
+
+
+
+    
+    Node* t;
+    while (head != nullptr) {
+        t = head;
+        head = head->next;
+        delete t;
+    }
+
+    return 0;
+}
